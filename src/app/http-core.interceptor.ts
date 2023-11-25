358d4b8c8f382dd77b2ec2aca5853563b902605f
Marcel Levinspuhl <marcel.levinspuhl@weareplanet.com>
2023-11-25 03:17:47 +0000

    cors working

---
+++ src/app/http-core.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core'
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http'
+import { Observable } from 'rxjs'
+
+@Injectable()
+export class HttpCoreInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    request = request.clone({
+
+      setHeaders: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin':'*'
+      }
+    })
+    return next.handle(request);
+    }
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/',
+  //apiUrl: 'http://localhost/',
+  apiUrl: 'https://52.18.129.98/',
   //apiUrl: 'http://52.18.129.98/',
   //apiUrl: 'https://localhost:7047',
   apiKey: ''
